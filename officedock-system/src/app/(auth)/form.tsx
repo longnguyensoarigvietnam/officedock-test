001a949ada47498eba11983ebdd7d1d4810fef36
Trung Nguyen <trung-nguyen@soarig.vn>
2025-01-15 14:15:05 +0700

    System: Revert button SSO and update some tooltips for Kanban

--- officedock-system/src/app/(auth)/form.tsx
+++ officedock-system/src/app/(auth)/form.tsx
@@ -9,6 +9,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import Input from '@components/common/Input';
 import Button from '@components/common/Button';
 import Checkbox from '@components/common/Checkbox';
+import ButtonSSO from '@components/common/ButtonSSO';
 
 import { apiRouters, pageRouters } from '@constants/routers';
 import {
@@ -151,6 +152,14 @@ const LoginForm = () => {
         />
         <Button type="submit">ログイン</Button>
       </form>
+      <div className="flex gap-4 items-center">
+        <div className="flex-grow h-[1px] bg-gray-200"></div>
+        <p className="text-sm">または</p>
+        <div className="flex-grow h-[1px] bg-gray-200"></div>
+      </div>
+      <div className="flex flex-col w-full gap-2 -mt-2">
+        <ButtonSSO provider="google" text="Googleでログイン" />
+      </div>
       <div className="flex flex-col justify-center items-center gap-2">
         <div className="flex items-center text-gray-500">
           <p>パスワードを忘れた方は</p>
