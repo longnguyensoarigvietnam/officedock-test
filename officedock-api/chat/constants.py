985d15803eb0cdd60e9c39dce84567c2b0b50dea
Hieu Vo <hieu-vo@soarig.vn>
2025-02-12 11:58:17 +0900

    API: Implement filter list chatroom by type

--- officedock-api/chat/constants.py
+++ officedock-api/chat/constants.py
@@ -67,8 +67,13 @@ class TypeChatGroup(EnumChoices):
     TypeChatGroup constants.
     """
 
+    # FIXME: Remove NOTIFY and CHAT later when if not split chatroom and notify group
     NOTIFY = "NOTIFY"
     CHAT = "CHAT"
+
+    GROUP = "GROUP"
+    PRIVATE = "PRIVATE"
+    UNREAD = "UNREAD"
 
 
 USER_ACTION_GROUP = "{}_user_action_group"
