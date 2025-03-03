8b39bd1228447ee9784cdc3148d26dd3ef6a06d5
Hieu Vo <hieu-vo@soarig.vn>
2025-03-03 16:12:14 +0900

    API: Refactor logic delete remind

--- officedock-api/tasks/apis.py
+++ officedock-api/tasks/apis.py
@@ -533,7 +533,7 @@ class TaskViewSet(
                             }
                         )
 
-        if serializer_data.get("deadline") and remind_countdown and remind_type:
+        if serializer_data.get("deadline"):
             serializer_data["remind_at"] = calculate_new_time(
                 serializer_data["deadline"], remind_countdown, remind_type
             )
