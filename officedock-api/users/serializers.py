c1b358520fd5cc5161ee34f7d41e583f0dca5fa5
Long Nguyen <long-nguyen@soarig.vn>
2025-04-12 13:06:55 +0700

    API: Update validate password

--- officedock-api/users/serializers.py
+++ officedock-api/users/serializers.py
@@ -68,8 +68,8 @@ class BaseUserSerializer(serializers.ModelSerializer):
         """
         Validate user password
         """
-
-        password_validation.validate_password(value, self.instance)
+        if value:
+            password_validation.validate_password(value)
         return value
 
     def validate_email(self, value):
@@ -632,7 +632,7 @@ class SystemUserInviteSerializer(serializers.ModelSerializer):
         Validate user password
         """
         if value:
-            password_validation.validate_password(value, self.instance)
+            password_validation.validate_password(value)
         return value
 
     def validate_email(self, value):
@@ -703,8 +703,8 @@ class ResetPasswordSerializer(serializers.Serializer):
         """
         Validate new password.
         """
-
-        password_validation.validate_password(value, self.instance)
+        if value:
+            password_validation.validate_password(value)
         return value
 
 
@@ -732,8 +732,8 @@ class ChangePasswordSerializer(serializers.Serializer):
         """
         Validate new password.
         """
-
-        password_validation.validate_password(value, self.instance)
+        if value:
+            password_validation.validate_password(value)
         return value
 
 
