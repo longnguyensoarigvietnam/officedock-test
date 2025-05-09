6fc02e46385e71b2a87c0b68ec29448c9ada9f8c
Truong Ngo <truong-ngo@soarig.vn>
2025-05-09 19:28:28 +0900

    System: Update sort no setting team

--- officedock-system/src/app/(main)/daily-report-team/[id]/daily-report-detail-board.tsx
+++ officedock-system/src/app/(main)/daily-report-team/[id]/daily-report-detail-board.tsx
@@ -1264,7 +1264,11 @@ const DailyReportDetailBoard = () => {
                 <div className="bg-transparent p-1">
                   <div className="w-12">
                     <Input
-                      disabled={!isPermissionAction || isRowParent}
+                      disabled={
+                        !isPermissionAction ||
+                        isRowParent ||
+                        row.original.isRunning
+                      }
                       defaultValue={
                         isRowParent
                           ? resultParentDuration?.pausedAt
