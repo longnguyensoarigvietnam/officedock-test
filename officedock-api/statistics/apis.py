a15b17204cec5593623234390cd57e7211e447e1
Hieu Vo <hieu-vo@soarig.vn>
2025-06-05 15:40:36 +0900

    API: Refactor logic get team statistic category by param user

--- officedock-api/statistics/apis.py
+++ officedock-api/statistics/apis.py
@@ -1211,6 +1211,7 @@ class OrganizationStatisticViewSet(BaseAPIViewSet):
             )
         else:
             users = instance.users.all()
+        org_users = instance.users.all()
         from_date = datetime.strptime(from_date, BASE_DATE_FORMAT).date()
         end_date = datetime.strptime(end_date, BASE_DATE_FORMAT).date()
         start_of_day = datetime.combine(from_date, time.min)
@@ -1221,14 +1222,14 @@ class OrganizationStatisticViewSet(BaseAPIViewSet):
         durations = get_list_durations_by_users(
             start_of_day,
             end_of_day,
-            users,
+            org_users,
             organizations=[instance],
             tags=tag_ids,
         )
 
         tasks, events = get_list_models(durations)
         total_duration, category_list = process_per_user(
-            users,
+            org_users,
             tasks,
             events,
             start_of_day,
@@ -1566,14 +1567,12 @@ class OrganizationStatisticViewSet(BaseAPIViewSet):
             users = User.objects.filter(id__in=users).all()
         else:
             return self.response_ok()
-        org_users = organization.users.all()
-
         tag_ids = split_id_from_string(tag_ids_param)
 
         durations = get_list_durations_by_users(
             start_of_day,
             end_of_day,
-            org_users,
+            users,
             [organization.id],
             tags=tag_ids,
         )
@@ -1581,51 +1580,27 @@ class OrganizationStatisticViewSet(BaseAPIViewSet):
         ranges = split_ranges(
             from_date, end_date, trim_whitespace(statistic_by)
         )
-        tasks, events = get_list_models(durations)
         data = []
-        total_duration, category_list = process_per_user(
-            org_users,
-            tasks,
-            events,
-            start_of_day,
-            end_of_day,
-            durations=durations,
-        )
-        if not durations.exists() or category_list is None:
-            return self.response_ok(data)
-        for category in category_list:
-            cat_percent = percentage_calculation_of_duration(
-                total_duration.total_seconds(),
-                category["duration"].total_seconds(),
-            )
-            cat_data = {
-                "id": category["category_id"],
-                "name": category["category_name"],
-                "total_duration": format_duration(category["duration"]),
-                "percent": min(round(cat_percent), 100),
-                "users": [],
-            }
-            for user in users:
-                filter_durations = get_list_durations_by_users(
-                    durations=durations,
-                    large_id=large_category_id,
-                    medium_id=medium_category_id,
-                    small_id=small_category_id,
-                    users=[user],
-                )
-                user_total_duration = get_total_durations(filter_durations)
-                user_durations = self._get_durations_by_range(
-                    filter_durations, ranges, durations
-                )
-                cat_data["users"].append(
-                    {
-                        "user": CreationDataUserSerializer(user).data,
-                        "total_duration": format_duration(user_total_duration),
-                        "durations": user_durations,
-                    }
-                )
 
-            data.append(cat_data)
+        for user in users:
+            filter_durations = get_list_durations_by_users(
+                durations=durations,
+                large_id=large_category_id,
+                medium_id=medium_category_id,
+                small_id=small_category_id,
+                users=[user],
+            )
+            user_total_duration = get_total_durations(filter_durations)
+            user_durations = self._get_durations_by_range(
+                filter_durations, ranges, durations
+            )
+            data.append(
+                {
+                    "user": CreationDataUserSerializer(user).data,
+                    "total_duration": format_duration(user_total_duration),
+                    "durations": user_durations,
+                }
+            )
 
         return self.response_ok(data)
 
