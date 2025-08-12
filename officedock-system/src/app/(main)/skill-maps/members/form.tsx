eec0662546d6f921dddf75806b640689cdbefafd
Trung Nguyen <trung-nguyen@soarig.vn>
2025-08-12 17:23:57 +0900

    System: Show correct today style in calendar

--- officedock-system/src/app/(main)/skill-maps/members/form.tsx
+++ officedock-system/src/app/(main)/skill-maps/members/form.tsx
@@ -143,7 +143,7 @@ export const SkillMapByMembersDetail = ({
                       className={`
                         text-[#77858F] bg-[#F8FAFC] text-xs font-medium py-3 max-w-[100%] truncate 
                         ${index !== headerGroup.headers.length - 1 ? 'border-r-[1px]' : ''}
-                        ${isSticky ? 'sticky left-0 z-20 bg-white' : ''}
+                        ${isSticky ? 'sticky left-0 z-20' : ''}
                       `}>
                       {flexRender(
                         header.column.columnDef.header,
