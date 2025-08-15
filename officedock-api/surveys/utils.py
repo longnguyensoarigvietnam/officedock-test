7dc797b3e1d5c5fe6184bc53ffde1088eaf0ff27
Long Nguyen <long-nguyen@soarig.vn>
2025-08-15 17:33:32 +0700

    API: Handle API get unanswered survey count

--- officedock-api/surveys/utils.py
+++ officedock-api/surveys/utils.py
@@ -1,7 +1,19 @@
 from datetime import datetime
 from django.utils.timezone import now
 
+from surveys.models import UsersViewedSurveys
+
 
 def is_open_survey(end_at: datetime):
     """Check survey is open"""
     return end_at > now()
+
+
+def view_survey_result(survey, current_user):
+    """Current user view survey result"""
+
+    UsersViewedSurveys.objects.get_or_create(
+        company_id=current_user.company_id,
+        survey=survey,
+        user=current_user,
+    )
