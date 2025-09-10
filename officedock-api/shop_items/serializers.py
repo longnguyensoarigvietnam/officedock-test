788e989d052749b9b8244e1686f24f3dbd27c2ae
Hieu Vo <hieu-vo@soarig.vn>
2025-09-10 11:28:45 +0900

    API: Implement logic check is own item in shop

--- officedock-api/shop_items/serializers.py
+++ officedock-api/shop_items/serializers.py
@@ -10,9 +10,20 @@ class ShopItemSerializer(serializers.ModelSerializer):
     Serializer for shop items
     """
 
+    is_owned = serializers.SerializerMethodField()
+
     class Meta:
         model = ShopItems
         fields = "__all__"
+        extra_fields = ["is_owned"]
+
+    def get_is_owned(self, obj):
+        request = self.context.get("request")
+        if request.user:
+            return UserItems.objects.filter(
+                user=request.user, item=obj
+            ).exists()
+        return False
 
 
 class GroupedItemSerializer(serializers.Serializer):
