87f5bbd85d7ef464f248d0a599c899cdb62d5d11
Hieu Vo <hieu-vo@soarig.vn>
2025-09-16 12:00:37 +0900

    API: Fixed bug search message in bookmark room

--- officedock-api/shop_items/serializers.py
+++ officedock-api/shop_items/serializers.py
@@ -8,17 +8,25 @@ class ShopItemSerializer(serializers.ModelSerializer):
     Serializer for shop items
     """
 
-    is_owned = serializers.SerializerMethodField()
+    is_owned = serializers.SerializerMethodField(read_only=True)
+    is_equipped = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = ShopItems
         fields = "__all__"
-        extra_fields = ["is_owned"]
+        extra_fields = ["is_owned", "is_equipped"]
 
     def get_is_owned(self, obj):
         user = self.context.get("user")
         if user:
             return UserItems.objects.filter(user=user, item=obj).exists()
+        return False
+
+    def get_is_equipped(self, obj):
+        user = self.context.get("user")
+        if user:
+            user_item = UserItems.objects.filter(user=user, item=obj).first()
+            return user_item.is_equipped if user_item else False
         return False
 
 
