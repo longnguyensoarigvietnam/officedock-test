10df8ae164279bfebbfc033969a100e1e0e06caa
Long Nguyen <long-nguyen@soarig.vn>
2025-10-24 19:05:39 +0900

    API: Show exchangeable coin of user in dotmoney

--- officedock-api/dotmoney/apis.py
+++ officedock-api/dotmoney/apis.py
@@ -143,7 +143,7 @@ class DotMoneyViewSet(BaseAPIViewSet):
         user = request.user
         user_id = user.id
         user_name = user.full_name
-        balance = user.exchangeable_coin
+        balance = min(user.coin, user.exchangeable_coin)
 
         # Build full exchange URL using DotMoney service
         full_url = DotMoneyService().build_exchange_url(
