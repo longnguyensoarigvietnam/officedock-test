6044e0555b5887d1e8fd32227e0b6232e16040f4
truongngosoarigvietnam <truong-ngo@soarig.vn>
2025-12-08 12:28:28 +0900

    System: Fix error build with skill map

--- officedock-system/src/app/(main)/skill-map/map.tsx
+++ officedock-system/src/app/(main)/skill-map/map.tsx
@@ -105,7 +105,7 @@ const SkillMap = () => {
           skillMapByOrganizations.map((skillMap, index) => (
             <SkillMapByOrganizationPanel
               key={index}
-              isMyOrg={skillMap.hasAssigned}
+              isMyOrg={skillMap.isDeleted}
               skillMapDetail={skillMap}
               userId={skillMapInfo?.user.id || 0}
             />
