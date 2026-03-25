fb70a3c15e954d4760e5767de0bba5543717932a
Long Nguyen <long-nguyen@soarig.vn>
2026-03-25 08:15:59 +0700

    API: Update export data multi user

--- officedock-api/migrate-staging.sh
+++ officedock-api/migrate-staging.sh
@@ -4,6 +4,3 @@ python manage.py migrate
 python manage.py seed_data_role
 python manage.py seed_data_admin
 python manage.py seed_data_task_status
-
-# Remove after deploy staging
-python manage.py update_company_temporary_usage_status --company-id=13
